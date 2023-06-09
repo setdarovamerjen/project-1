42cc6a01cae81bf5cbb84798582780a85c4424b9
setdarovamerjen <setdarovamerjen@gmail.com>
2023-06-09 18:18:11 -0500

    Initial commit

---
+++ src/test/java/pages/GoogleHomePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleHomePage {
+
+    public GoogleHomePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+}
